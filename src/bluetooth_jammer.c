a4900613f703346d881c080295ce1f10f30662d5
nikki <124477449+variendarkgirl@users.noreply.github.com>
2025-03-23 23:59:28 +0500

    Update bluetooth_jammer.c

--- src/bluetooth_jammer.c
+++ src/bluetooth_jammer.c
@@ -6,7 +6,6 @@ void init_bluetooth() {
     printf("[*] Initializing Bluetooth interface...\n");
     system("hciconfig hci0 up");
 }
-
 void disable_bluetooth() {
     printf("[*] Disabling Bluetooth interface...\n");
     system("hciconfig hci0 down");
